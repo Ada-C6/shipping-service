902ada5ac67dc8ef1efc66e4290aac3e832b7952
Qlwentt <qlwentt@gmail.com>
2016-11-07 15:46:30 -0800

    configures auto load for lib

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module ShippingService
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    #ads lib folder to autoload paths
+    config.autoload_paths << config.root.join("lib")
   end
 end
