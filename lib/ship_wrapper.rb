bf9d19ae5c2b8ae673e873626562c9586da8be5e
laurenfb <laurenfriesbrink@gmail.com>
2016-11-08 15:39:57 -0800

    added the beginnings of shipwrapper to communicate with shipping source APIs

--- lib/ship_wrapper.rb
+++ lib/ship_wrapper.rb
@@ -5,29 +5,34 @@ require 'active_shipping'
 class ShipWrapper
 
   ORIGIN =  ActiveShipping::Location.new(country: 'US', state: 'WA', city: 'Seattle', zip: '98119')
-  USPS_LOGIN = ENV["UPS_USERNAME"]
+  USPS_LOGIN = ENV["USPS_USERNAME"]
 
   def self.get_rates(carrier, package, destination)
     case carrier.downcase
     when 'usps'
       usps = ActiveShipping::USPS.new(login: USPS_LOGIN)
       response = usps.find_rates(ORIGIN, destination, package)
-      usps_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     when 'fedex'
+      # this will be populated later. right now we are just trying with one single carrier.
     when 'ups'
+      # same as above
     else
-      "blah blah"
+      rates = []
     end
+    return rates #=> [["usps blah blah", 3404], ["usps overnight", 49383]]
   end
 
-  def self.make_package(weight, array_of_heights)
-    package = ActiveShipping::Package.new(weight, array_of_heights, units: :imperial)
-    return package
-  end
 
-  def self.make_destination(country, state, city, zip)
-    destination = ActiveShipping::Location.new(country: country, state: state, city: city, zip: zip)
-    return destination
-  end
+# this should go in the controller
+  # def self.make_package(weight, array_of_dimensions)
+  #   package = ActiveShipping::Package.new(weight, array_of_dimensions, units: :imperial)
+  #   return package
+  # end
+  #
+  # def self.make_destination(country, state, city, zip)
+  #   destination = ActiveShipping::Location.new(country: country, state: state, city: city, zip: zip)
+  #   return destination
+  # end
 
 end
