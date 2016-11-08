b50ebbff0568f4c3c8cd5d45b088812b91553f74
Esther Ng <esther.hc.ng@gmail.com>
2016-11-08 12:28:11 -0800

    Created some test stubs

--- test/lib/quote_test.rb
+++ test/lib/quote_test.rb
@@ -4,4 +4,22 @@ class QuoteTest < ActiveSupport::TestCase
   test "that tests are working" do
     assert true
   end
+
+  test "should #initialize with weight, origin, destination, dimensions" do
+    weight = 3
+    origin = { country: 'US', state: 'WA', city: 'Seattle', zip: '98151' }
+    destination = { country: 'US', state: 'CA', city: 'Los Angeles', zip: '90032' }
+    dimensions = [15, 10, 4.5]
+    q = Quote.new(weight, origin, destination, dimensions)
+    # assert_equal
+  end
+
+  test "on #intialize should create new package, origin and destination with Active Shipping gem" do
+
+  end
+
+  test "calling .ups will return a hash of rates from UPS with different service names" do
+    
+  end
+
 end
