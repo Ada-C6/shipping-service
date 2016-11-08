d79ba2b95d3ef58b3801739d70b8ef7cecca6184
Emily Abrash <emabrash@gmail.com>
2016-11-08 12:55:44 -0800

    got UPS rates to display

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,6 +1,12 @@
 class ShipmentsController < ApplicationController
 
   def index
+    package = ActiveShipping::Package.new(params[:weight].to_f*16, [12,12,12], units: :imperial)
+    sender = ActiveShipping::Location.new(country: 'US', zip: params[:origin_zip].to_i)
+    receiver = ActiveShipping::Location.new(country: 'US', zip: params[:destination_zip].to_i)
+    ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
+    response = ups.find_rates(sender, receiver, package)
+    render :json => response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_range]}, :status => :ok
   end
 
   def show
