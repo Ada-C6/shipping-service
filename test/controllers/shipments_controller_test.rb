8c1d02783e7d9eecab9ed874919b55d1443217e5
KellyPT <trankphuong@gmail.com>
2016-11-08 11:10:48 -0800

    Added 'active-shipping' gem. Generated controllers. Started on controller test.

---
+++ test/controllers/shipments_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class ShipmentsControllerTest < ActionController::TestCase
+  setup do
+    @request.headers['Accept'] = Mime::JSON
+    @request.headers['Content-Type'] = Mime::JSON.to_s
+  end
+
+
+end
