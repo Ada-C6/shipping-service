55559199bf17d61431763b598d3b3c4922bd717b
Emily Abrash <emabrash@gmail.com>
2016-11-08 15:32:11 -0800

    added logging

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,10 +1,16 @@
+require 'timeout'
+
 class ShipmentsController < ApplicationController
 
   def index
     begin
-      render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
+      status = Timeout::timeout(10){
+        render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
+      }
     rescue ActiveShipping::ResponseError => e
       render :json => {error: "THERE WAS AN ISSUE. HIGHLY QUESTIONABLE."}, :status => '404'
+    rescue Timeout::Error
+      render :json => {error: "THIS IS TAKING WAY TOO LONG."}, :status => '404'
     end
   end
 
