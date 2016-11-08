6c0237a2c4bec8e5aa47eaa66685289e37002bd0
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-08 12:03:04 -0800

    Location model and tests developed via TDD

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,13 @@
 class Location < ActiveRecord::Base
+
+  validates :country, :state, :city, :zip, presence: true
+
+  attr_reader :country, :state, :city, :zip
+
+  def initialize (country, state, city, zip)
+    @country = country
+    @state = state
+    @city = city
+    @zip = zip
+  end
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should have all required fields" do
+    location = Location.new(country = "US", state = "California", city = "Los Angeles", zip = 90215)
+
+    assert location.valid?
+  end
+
+  test "Not valid location should result in incomplete-based errors" do
+    location = Location.new(country = nil, state = nil, city = nil, zip = nil)
+
+    assert_not location.valid?
+    assert_includes location.errors, :country
+    assert_includes location.errors, :state
+    assert_includes location.errors, :city
+    assert_includes location.errors, :zip
+  end
 end
