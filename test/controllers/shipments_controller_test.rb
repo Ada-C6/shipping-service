fa51338d8ec88afae47f71133dfdf498011544d2
KellyPT <trankphuong@gmail.com>
2016-11-08 11:28:55 -0800

    created secrets.yml in config folder. Added gem 'rails-api'.

--- test/controllers/shipments_controller_test.rb
+++ test/controllers/shipments_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+# require 'active_shipping'
 
 class ShipmentsControllerTest < ActionController::TestCase
   setup do
@@ -6,6 +7,11 @@ class ShipmentsControllerTest < ActionController::TestCase
     @request.headers['Content-Type'] = Mime::JSON.to_s
   end
 
-  
-
+  # Should return a list of shipment services and their corresponding rates and delivery estimates
+  test "can get #index" do
+    get :index
+    assert_response :success
+    assert_match 'application/json', response.header['Content-Type']
+    # assert_match Mime::JSON.to_s, response.header['Content-Type']
+  end
 end
