f8c7a608b5c2129b377f34063e055c600aa9aa89
Emily Abrash <emabrash@gmail.com>
2016-11-08 14:11:36 -0800

    Errors added

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -1,10 +1,16 @@
 class ShipmentsController < ApplicationController
 
   def index
-    render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
+    begin
+      render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
+    rescue ActiveShipping::ResponseError => e
+      render :json => {error: "THERE WAS AN ISSUE. HIGHLY QUESTIONABLE."}, :status => '404'
+    end
   end
 
   def show
   end
 
 end
+
+# e.response.params["Response"]["Error"].first["ErrorDescription"]
