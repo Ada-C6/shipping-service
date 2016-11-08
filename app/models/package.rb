f4231edc4117b6906fa00495d1b9d4ef1d181acd
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-08 12:15:44 -0800

    Package model and tests developed via TDD

--- app/models/package.rb
+++ app/models/package.rb
@@ -1,3 +1,14 @@
 class Package < ActiveRecord::Base
 
+  validates :weight, :length, :width, :height, presence: true
+
+  attr_reader :weight, :length, :width, :height
+
+  def initialize (weight, length, width, height)
+    @weight = weight
+    @length = length
+    @width = width
+    @height = height
+  end
+
 end
