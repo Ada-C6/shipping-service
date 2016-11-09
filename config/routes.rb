d59ff9c49226ee5d2b130b09cc49a6803b2c12c3
LaurenSky <ljherrer@gmail.com>
2016-11-09 15:23:14 -0800

    fixed route name from last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   get '/shipping_quotes', to: 'shipments#index', as: 'shipping_quotes'
-  post '/shipping_quotes', to: 'shipments#index', as: 'shipping_quotes'
+  post '/shipping_quotes', to: 'shipments#index'
   get '/shipment', to: 'shipments#show', as: 'shipment'
 
 end
