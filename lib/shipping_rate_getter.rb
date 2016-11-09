4e828db75bf26dae4c38a1d5a8aad45b7291e04c
Elle Vargas <vargas.elle@gmail.com>
2016-11-09 14:19:32 -0800

    added database parsing for each carrier - will dry up later

--- lib/shipping_rate_getter.rb
+++ lib/shipping_rate_getter.rb
@@ -19,17 +19,58 @@ class ShippingRateGetter
 
     results_hash["UPS"] = ups_response_trimmed
 
+    results_hash["UPS"].each do |line|
+      method = line[0]
+      cost = line[1]
+
+      if line[2] != []
+        delivery_estimate = line[2].first
+      else
+        delivery_estimate = nil
+      end
+
+      request.results << Result.create(price: cost, provider: "UPS", service: method, delivery_est: delivery_estimate)
+    end
+
+
     usps = ActiveShipping::USPS.new(login: ENV['USPS_LOGIN'])
     usps_response = usps.find_rates(sender, receiver, package)
     usps_response_trimmed = usps_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_range]}
 
     results_hash["USPS"] = usps_response_trimmed
 
+    results_hash["USPS"].each do |line|
+      method = line[0]
+      cost = line[1]
+
+      if line[2] != []
+        delivery_estimate = line[2].first
+      else
+        delivery_estimate = nil
+      end
+
+      request.results << Result.create(price: cost, provider: "USPS", service: method, delivery_est: delivery_estimate)
+    end
+
+
     fedex = ActiveShipping::FedEx.new(login: ENV['FEDEX_LOGIN'], password: ENV['FEDEX_PASSWORD'], key: ENV['FEDEX_KEY'], account: ENV['FEDEX_ACCOUNT'], test: true)
     fedex_response = fedex.find_rates(sender, receiver, package)
     fedex_response_trimmed = fedex_response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_range]}
 
     results_hash["Fedex"] = fedex_response_trimmed
+
+    results_hash["Fedex"].each do |line|
+      method = line[0]
+      cost = line[1]
+
+      if line[2] != []
+        delivery_estimate = line[2].first
+      else
+        delivery_estimate = nil
+      end
+
+      request.results << Result.create(price: cost, provider: "Fedex", service: method, delivery_est: delivery_estimate)
+    end
 
     return results_hash
 
