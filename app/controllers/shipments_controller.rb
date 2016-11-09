b711197b2d7e31b46628bc28fffef278eed853b5
Elle Vargas <vargas.elle@gmail.com>
2016-11-09 14:52:21 -0800

    error messages for weight, timeout, and catch-all now working

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -8,9 +8,9 @@ class ShipmentsController < ApplicationController
         render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
       }
     rescue ActiveShipping::ResponseError => e
-      render :json => {error: "THERE WAS AN ISSUE. HIGHLY QUESTIONABLE."}, :status => '404'
+      render :json => {error: "UH-OH. LOOKS LIKE SOMETHING'S NOT QUITE RIGHT. PERHAPS YOUR ZIP CODES ARE OFF BY ONE? PLEASE NOTE: PACKAGES HAVE A WEIGHT LIMIT OF 150 LBS."}, :status => '404'
     rescue Timeout::Error
-      render :json => {error: "THIS IS TAKING WAY TOO LONG."}, :status => '404'
+      render :json => {error: "THIS IS TAKING WAY TOO LONG. SORRY, BUT THIS IS NOW A TIMEOUT ERROR. TRY GOING BACK AND REFRESHING THE PAGE AND SUBMITTING YOUR REQUEST AGAIN."}, :status => '404'
     end
   end
 
