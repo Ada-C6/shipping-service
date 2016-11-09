2424163b417c75ee319a8d62bcd77958c1d39f28
Mary Morrison <marymorrison15@gmail.com>
2016-11-09 12:28:46 -0800

    added tests for usps method and fixed actual method, realized .env file is useless, must hard code login info, got simplecov up and running in test_helper file

--- test/models/shipping_test.rb
+++ test/models/shipping_test.rb
@@ -7,13 +7,14 @@ class ShippingTest < ActiveSupport::TestCase
   # end
 
   test "should create a destination" do
-    city = shippings(:one).city
-    state = shippings(:one).state
-    zip = shippings(:one).zip
-    country = shippings(:one).country
+    city = shippings(:destination_one).city
+    state = shippings(:destination_one).state
+    zip = shippings(:destination_one).zip
+    country = shippings(:destination_one).country
     # address = country: '#{country}', state: '#{state}', city: '#{city}', zip: '#{zip}'"
     location = Shipping.destination(country, state, city, zip)
     assert_not_nil location
+    assert_kind_of ActiveShipping::Location, location
   end
 
   test "should return array of packages" do
@@ -23,4 +24,25 @@ class ShippingTest < ActiveSupport::TestCase
     assert_kind_of Array, packages
     assert_equal packages.count, 5
   end
+
+  test "usps method should return array of arrays with service name and cost" do
+    ORIGIN = ActiveShipping::Location.new(country: 'US', state: 'WA', city: 'Seattle', zip: '98122')
+    origin = ORIGIN
+
+    city = shippings(:destination_two).city
+    state = shippings(:destination_two).state
+    zip = shippings(:destination_two).zip
+    country = shippings(:destination_two).country
+    destination = Shipping.destination(country, state, city, zip)
+
+    weights_string = shippings(:weights_two).weights
+    packages = Shipping.create_packages(weights_string)
+
+    rates = Shipping.usps(origin, destination, packages)
+    assert_not_nil rates
+    assert_kind_of Array, rates
+    assert_kind_of Array, rates[0]
+    assert_equal rates.count, 6
+  end
+
 end
