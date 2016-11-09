fc51352211c67321c62e3569ba7796d18731250d
Allison Hoke <allisonhoke@gmail.com>
2016-11-09 15:47:26 -0800

    created shipment class that brings in petsy info and outputs info that is usable in ActiveShipping gem

---
+++ lib/shipment.rb
@@ -0,0 +1,15 @@
+class Shipment
+  attr_reader :package, :origin, :destination
+  
+  def initialize(shipment_info_hash)
+    weight = shipment_info_hash[:weight] * 16 #we are assuming petsy will add all package weights for us
+    dest_country = shipment_info_hash[:country]
+    dest_state = shipment_info_hash[:state]
+    dest_city = shipment_info_hash[:city]
+    dest_zip = shipment_info_hash[:zip] #we are assuming this is coming in as a string
+
+    @package = ActiveShipping::Package.new(weight, [15, 10, 4.5], units: :imperial)
+    @origin = ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", zip: "98101")
+    @destination = ActiveShipping::Location.new(dest_country, dest_state, dest_city, dest_zip)
+  end
+end
