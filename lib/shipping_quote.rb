3bd917d10bf96745b5cafa28603b8fe3db2c2db5
Alma Maizels <aymaiz@yahoo.com>
2016-11-09 13:35:28 -0800

    request rates for one carrier implemented

--- lib/shipping_quote.rb
+++ lib/shipping_quote.rb
@@ -5,12 +5,12 @@ class ShippingQuote
 UPS_LOGIN = ENV['ACTIVESHIPPING_UPS_LOGIN']
 UPS_KEY = ENV['ACTIVESHIPPING_UPS_KEY']
 UPS_PASSWORD = ENV['ACTIVESHIPPING_UPS_PASSWORD']
+USPS_LOGIN = ENV['ACTIVESHIPPING_USPS_LOGIN']
 
-  def initialize(package, origin, destination)
+  def initialize(package, origin, destination, service_quotes = [])
     @package = package
     @origin = origin
     @destination = destination
-    @service_quotes = []
   end
 
   #Method will return service_quotes from the requested carrier (through an ActiveShipping object) as an array of arrays
@@ -22,11 +22,26 @@ UPS_PASSWORD = ENV['ACTIVESHIPPING_UPS_PASSWORD']
 
       # UPS rates/quotes [ [],[],... ]
       return response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    elsif carrier == "usps"
+      usps = ActiveShipping::USPS.new(login: USPS_LOGIN)
+      response = usps.find_rates(@origin, @destination, @package)
+      return response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     end
   end
 
   # method putting quotes together parcel.carrier_quotes([ups,usps,canadapost] )
+  def carrier_quotes(carriers)
+    quotes = []
+    carriers.each do |carrier|
+      requesting_quote(carrier).each do |quote|
+        quotes << quote
+      end
+    end
+    return quotes
+  end
 
-
+  # def self.build_quote #(package, origin, destination, service_quotes =[])
+  #   return ShippingQuote.new(@package, @origin, @destination, quotes)
+  # end
 
 end
--- test/lib/shipping_quote_test.rb
+++ test/lib/shipping_quote_test.rb
@@ -5,7 +5,7 @@ class ShippingQuoteTest < ActiveSupport::TestCase
   ADA = {country: 'US',state: 'WA', city: 'Seattle', zip: '98101'}
 
 ### Tests for initializing ###
-  test "should initilize a quote with the required arguments" do
+  test "should initialize a quote with the required arguments" do
     VCR.use_cassette("active_shipping") do
       package = ActiveShipping::Package.new(7.5 * 16,           # weight
                                             [12,12,12],         # dimensions
@@ -23,7 +23,7 @@ class ShippingQuoteTest < ActiveSupport::TestCase
 
 ### Parcel wrapping ###
 
-# carriers supported: UPS
+# carriers supported: UPS, USPS
   test "#requesting_quote(carrier) should return an instance of ShippingQuote from the different carriers" do
     VCR.use_cassette("active_shipping") do
       carrier = "ups"
@@ -32,9 +32,7 @@ class ShippingQuoteTest < ActiveSupport::TestCase
                                             units: :imperial)   # options
       origin = ActiveShipping::Location.new(PETSY)
       destination = ActiveShipping::Location.new(ADA)
-
       parcel = ShippingQuote.new(package, origin, destination)
-
       quotes = parcel.requesting_quote(carrier) # array of arrays
 
       assert Array, quotes
@@ -48,9 +46,47 @@ class ShippingQuoteTest < ActiveSupport::TestCase
   # test "#requesting_quote(carrier) accepts only arguments with the required data" do
   # end
 
-  test "#requesting_quote will only return valid data" do
+  test "#requesting_quote will only return valid data from ups" do
     VCR.use_cassette("active_shipping") do
       carrier = "ups"
+      package = ActiveShipping::Package.new(7.5 * 16,           # weight
+                                            [12,12,12],         # dimensions
+                                            units: :imperial)   # options
+      origin = ActiveShipping::Location.new(PETSY)
+      destination = ActiveShipping::Location.new(ADA)
+      parcel = ShippingQuote.new(package, origin, destination)
+
+      quotes = parcel.requesting_quote(carrier) # array of arrays
+
+      assert_not_nil quotes
+
+      quotes.each do |quote|
+        assert String, quote[0]
+        assert Integer, quote[1]
+      end
+    end
+  end
+
+  test "requesting_quote from unknown carrier will return nil" do
+      VCR.use_cassette("active_shipping") do
+        carrier = "Lucy's cargo"
+        package = ActiveShipping::Package.new(7.5 * 16,           # weight
+                                              [12,12,12],         # dimensions
+                                              units: :imperial)   # options
+        origin = ActiveShipping::Location.new(PETSY)
+        destination = ActiveShipping::Location.new(ADA)
+        parcel = ShippingQuote.new(package, origin, destination)
+
+        nil_quotes = parcel.requesting_quote(carrier)
+
+        assert_nil nil_quotes
+      end
+  end
+
+  # for each of the carriers, except ups above, only one test
+  test "#requesting_quote will only return valid data from usps" do
+    VCR.use_cassette("active_shipping") do
+      carrier = "usps"
       package = ActiveShipping::Package.new(7.5 * 16,           # weight
                                             [12,12,12],         # dimensions
                                             units: :imperial)   # options
@@ -62,13 +98,61 @@ class ShippingQuoteTest < ActiveSupport::TestCase
 
       quotes = parcel.requesting_quote(carrier) # array of arrays
 
+      assert_not_nil quotes
+      assert Array, quotes
+
       quotes.each do |quote|
-        assert String, quote[0].class
+        assert Array, quote
+        assert String, quote[0]
         assert Integer, quote[1]
       end
-      
-
     end
   end
+
+  # test "#carrier_quotes should return supported carriers quotes as an array of length = 2 arrays" do
+  #   VCR.use_cassette("active_shipping") do
+  #     petsy_carriers = ["ups", "usps"] #the carriers approved by our customer (petsy)
+  #     package = ActiveShipping::Package.new(7.5 * 16,           # weight
+  #                                           [12,12,12],         # dimensions
+  #                                           units: :imperial)   # options
+  #     origin = ActiveShipping::Location.new(PETSY)
+  #     destination = ActiveShipping::Location.new(ADA)
+  #
+  #     parcel = ShippingQuote.new(package, origin, destination)
+  #
+  #     quotes = parcel.carrier_quotes(petsy_carriers) # array of arrays
+  #
+  #     quotes.each do |quote|
+  #       assert_instance_of Array, quote
+  #       assert_equal 2, quote.length # not valid because we are also testing two carriers
+  #
+  #     end
+  #   end
+  # end
+
+  #first implement a second carrier then @todo : ->
+
+  # test "#carrier_quotes should return all the quotes from each carrier as an array of arrays" do
+  #   VCR.use_cassette("active_shipping") do
+  #     petsy_carriers = ["ups"] #the carriers approved by our customer (petsy)
+  #     package = ActiveShipping::Package.new(7.5 * 16,           # weight
+  #                                           [12,12,12],         # dimensions
+  #                                           units: :imperial)   # options
+  #
+  #     origin = ActiveShipping::Location.new(PETSY)
+  #     destination = ActiveShipping::Location.new(ADA)
+  #
+  #     parcel = ShippingQuote.new(package, origin, destination)
+  #     quotes = parcel.carrier_quotes(petsy_carriers) # array of arrays
+  #
+  #     assert_equal 6, quotes.length
+  #     #must implement a second carrier for this to work/fail
+  #     quotes.each do |quote|
+  #       assert Array, quote
+  #     end
+  #   end
+  # end
+
+
 
 end
