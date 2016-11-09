8a938f71ddeeb3b01d24965cef0117e7866f2b65
Esther Ng <esther.hc.ng@gmail.com>
2016-11-09 11:08:29 -0800

    First stab at create quotes controller method, also comment on how to handle in Petsy

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,12 @@
 class QuotesController < ApplicationController
   def create
-
+    weight = params["weight"]
+    destination = params["destination"]
+    quote = Quote.new(weight, nil, destination)
+    quotes = {ups: quote.ups, usps: quote.usps}
+    render json: { quotes: quotes }
   end
 end
+
+
+# in Petsy, HTTParty.post(body: {}, headers: {"Content-Type": application/json'})
