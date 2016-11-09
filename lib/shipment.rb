abd5f6c1bbe9832e622f57ca4c1c67a5c8a72042
Allison Hoke <allisonhoke@gmail.com>
2016-11-09 15:53:07 -0800

    added require and adjusted destination creation

--- lib/shipment.rb
+++ lib/shipment.rb
@@ -1,6 +1,8 @@
+require 'active_shipping'
+
 class Shipment
   attr_reader :package, :origin, :destination
-  
+
   def initialize(shipment_info_hash)
     weight = shipment_info_hash[:weight] * 16 #we are assuming petsy will add all package weights for us
     dest_country = shipment_info_hash[:country]
@@ -10,6 +12,6 @@ class Shipment
 
     @package = ActiveShipping::Package.new(weight, [15, 10, 4.5], units: :imperial)
     @origin = ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", zip: "98101")
-    @destination = ActiveShipping::Location.new(dest_country, dest_state, dest_city, dest_zip)
+    @destination = ActiveShipping::Location.new(country: dest_country, state: dest_state, city: dest_city, zip: dest_zip)
   end
 end
