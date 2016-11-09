a4657a5713a97e523b6ecb2c5846097ab56ea5b7
Johna N. Morris <johna.n.morris@gmail.com>
2016-11-08 16:06:10 -0800

    wrote tests for model validation, and destination instance method

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,5 +1,26 @@
 # require 'active_shipping'
 
 class Shipment < ActiveRecord::Base
+  validates :weight, presence: true
+  validates :height, presence: true
+  validates :length, presence: true
+  validates :width, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :country, presence: true
+  validates :zipcode, presence: true
+  validates :units, presence: true
+
+  def origin
+    # Normally this wouldn't be hardcoded, but the limitations of the
+    # project necessitate it.
+    return ActiveShipping::Location.new(country: "United States", state: "WA", city: "Seattle", postal_code: "98161")
+  end
+
+
+  #This method works for both origin and destination
+  def destination
+    return ActiveShipping::Location.new(country: country, state: state, city: city, postal_code: zipcode)
+  end
 
 end
--- test/models/shipment_test.rb
+++ test/models/shipment_test.rb
@@ -1,9 +1,190 @@
 require 'test_helper'
 
 class ShipmentTest < ActiveSupport::TestCase
-  test "a package will be created with valid weight, height, length, unit" do
-    package = ActiveShipping::Package.new(7.5 * 16,[15, 10, 4.5], units: :imperial)
-    assert_not_nil package
-    assert_instance_of Shipment, package
+
+
+  test "create a valid Shipment object" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert shipment.valid?
+  end
+
+  test "can't create a Shipment without a weight" do
+    ship_hash = {
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+
+      shipment = Shipment.new(ship_hash)
+      assert_not shipment.valid?
+      assert_includes shipment.errors, :weight
+  end
+
+  test "can't create a Shipment without a height" do
+    ship_hash = {
+      weight: 100.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :height
+  end
+
+  test "can't create a Shipment without a length" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :length
+
+
+  end
+
+  test "can't create a Shipment without a width" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :width
+  end
+
+  test "can't create a Shipment without a city" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+  shipment = Shipment.new(ship_hash)
+  assert_not shipment.valid?
+  assert_includes shipment.errors, :city
+  end
+
+  test "can't create a Shipment without a state" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      country: "US",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :state
+  end
+
+
+  test "can't create a Shipment without a country" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      zipcode: "89121",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :country
+  end
+
+  test "can't create a Shipment without a zipcode" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      units: "imperial"
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :zipcode
+  end
+
+  test "can't create a Shipment without unit" do
+    ship_hash = {
+      weight: 100.0,
+      height: 12.0,
+      length: 12.0,
+      width: 12.0,
+      city: "Las Vegas",
+      state: "NV",
+      country: "US",
+      zipcode: "89121",
+    }
+
+    shipment = Shipment.new(ship_hash)
+    assert_not shipment.valid?
+    assert_includes shipment.errors, :units
+  end
+
+  test "#destination returns a Location object" do
+    shipment = shipments(:one)
+    destination = shipment.destination
+    assert_instance_of ActiveShipping::Location, destination
+
+    assert_equal shipment.city, destination.city
+    assert_equal shipment.state, destination.state
+    assert_equal shipment.country, destination.country.name
+    assert_equal shipment.zipcode, destination.postal_code
   end
 end
