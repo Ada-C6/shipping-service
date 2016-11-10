1fd0b1b9abcd41e32c9c526fd1f30a65f3b89fc4
guineveresaenger <guineveresaenger@yahoo.com>
2016-11-10 15:18:28 -0800

    Added simplecov to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,15 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'simplecov'
+require 'minitest/reporters'
+
+SimpleCov.start
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
+  Minitest::Reporters.use!
   # Add more helper methods to be used by all tests here...
 end
