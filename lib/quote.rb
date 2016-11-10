3d8a965d90cbec3d376bc7529b85327539a9c82e
Allison Hoke <allisonhoke@gmail.com>
2016-11-10 09:09:50 -0800

    WIP

---
+++ lib/quote.rb
@@ -0,0 +1,12 @@
+class Quote
+  def initialize(shipment_info_hash)
+    @shipment = Shipment.new(shipment_info_hash)
+    @ups_rate = 
+  end
+
+
+def find_rates
+  ups.find_rates(@shipment.origin, @shipment.destination, a.)
+
+end
+end
