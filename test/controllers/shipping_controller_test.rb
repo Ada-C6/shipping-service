769a4322dfb1bf670ce2d7b59aa6ea0dece214c3
laurenfb <laurenfriesbrink@gmail.com>
2016-11-10 15:07:26 -0800

    tests for shipping controller

--- test/controllers/shipping_controller_test.rb
+++ test/controllers/shipping_controller_test.rb
@@ -48,4 +48,65 @@ class ShippingControllerTest < ActionController::TestCase
     assert_equal(body["error"], "must specify a carrier from #{ShipWrapper.valid_carriers.join(', ')}")
   end
 
+  test 'when :packages param is not present, should return a 400 bad request w/ error message' do
+    @params[:packages] = nil
+    post :quote, @params
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "missing packages field")
+  end
+
+  test 'when :packages param present but not an array, should return a 400 bad request w/ error message' do
+    @params[:packages] = "cat"
+    post :quote, @params
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "packages field must be an array of package hash")
+  end
+
+  test 'when :packages param is empty should return 400 w error msg' do
+    @params[:packages] = []
+    post :quote, @params
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "packages field must not be empty")
+  end
+
+  test 'when packages is correct except that its elements are not a hash, yet again return an error' do
+    @params[:packages] = [["length", "width", 100], ["cat", "blah"], 'blue']
+    post :quote, @params
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "packages field must be an array of package hash")
+  end
+
+  test 'when packages is missing an element, should get an error back' do
+    def helper_method
+      post :quote, @params
+      assert_response :bad_request
+      body = JSON.parse(response.body)
+      assert_instance_of(Hash, body)
+      assert_equal(body["error"], "package hash must have 'weight', 'height', 'length', 'width'")
+    end
+
+    @params[:packages][0][:weight] = nil
+    helper_method
+    @params[:packages][0][:weight] = 10
+    @params[:packages][0][:height] = nil
+    helper_method
+    @params[:packages][0][:height] = 10
+    @params[:packages][0][:length] = nil
+    helper_method
+    @params[:packages][0][:length] = 10
+    @params[:packages][0][:width] = nil
+    helper_method
+
+  end
+
+
+
 end
