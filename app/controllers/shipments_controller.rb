38294f75431ccfcf4d4749db0c7f90fa14bc7d6e
Elle Vargas <vargas.elle@gmail.com>
2016-11-10 09:45:35 -0800

    added e.message error response for debugging

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -8,7 +8,7 @@ class ShipmentsController < ApplicationController
         render :json => ShippingRateGetter.get_rates(params[:weight].to_f, params[:origin_zip].to_i, params[:destination_zip].to_i), :status => :ok
       }
     rescue ActiveShipping::ResponseError => e
-      render :json => {error: "UH-OH. LOOKS LIKE SOMETHING'S NOT QUITE RIGHT. PERHAPS YOUR ZIP CODES ARE OFF BY ONE? PLEASE NOTE: PACKAGES HAVE A WEIGHT LIMIT OF 150 LBS."}, :status => '404'
+      render :json => {error: "UH-OH. LOOKS LIKE SOMETHING'S NOT QUITE RIGHT. PERHAPS YOUR ZIP CODES ARE OFF BY ONE? PLEASE NOTE: PACKAGES HAVE A WEIGHT LIMIT OF 150 LBS.", details: e.message}, :status => '404'
     rescue Timeout::Error
       render :json => {error: "THIS IS TAKING WAY TOO LONG. SORRY, BUT THIS IS NOW A TIMEOUT ERROR. TRY GOING BACK AND REFRESHING THE PAGE AND SUBMITTING YOUR REQUEST AGAIN."}, :status => '404'
     end
