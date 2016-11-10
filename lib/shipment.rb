f2d632476aafd52e8b345ed8647df87b1899bcce
Allison Hoke <allisonhoke@gmail.com>
2016-11-10 13:21:22 -0800

    WIP

--- lib/shipment.rb
+++ lib/shipment.rb
@@ -14,6 +14,38 @@ class Shipment
     @origin = ActiveShipping::Location.new(country: "US", state: "WA", city: "Seattle", zip: "98101")
     @destination = ActiveShipping::Location.new(country: dest_country, state: dest_state, city: dest_city, zip: dest_zip)
   end
+
+  def usps_options
+    usps = ActiveShipping::USPS.new(login: ENV['USPS_LOGIN'])
+    response = usps.find_rates(self.origin, self.destination, self.package)
+    rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    return rates
+  end
+
+  def ups_options
+      ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
+      response = ups.find_rates(self.origin, self.destination, self.package)
+      rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+      return rates
+  end
+
+
+  # private
+  # def usps_rates
+  #     usps = ActiveShipping::USPS.new(login: ENV['USPS_LOGIN'])
+  #     response = usps.find_rates(@shipment.origin, @shipment.destination, @shipment.package)
+  #     rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  #     return rates
+  #     # this comes back as an array of arrays with all USPS shipping option quotes
+  # end
+  #
+  # def ups_rates
+  #     ups = ActiveShipping::UPS.new(login: ENV['UPS_LOGIN'], password: ENV['UPS_PASSWORD'], key: ENV['UPS_KEY'])
+  #     response = ups.find_rates(@shipment.origin, @shipment.destination, @shipment.package)
+  #     rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+  #     return rates
+  #     # this comes back as an array of arrays with all UPS shipping option quotes
+  # end
 end
 
 shipment_info_hash = {weight: 15, country: 'US', state: 'OH', city: 'Akron', zip: '44333' }
@@ -21,3 +53,7 @@ a = Shipment.new(shipment_info_hash)
 puts a.origin
 puts a.destination
 puts a.package.to_s
+ups = a.ups_options
+puts ups
+usps = a.usps_options
+puts usps
