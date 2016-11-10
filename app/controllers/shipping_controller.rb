6677d5673a07a37b0c6d923847a2e56f03e158cb
Dianne Laguerta <diannelaguerta@gmail.com>
2016-11-10 14:37:22 -0800

    having trouble making things work

--- app/controllers/shipping_controller.rb
+++ app/controllers/shipping_controller.rb
@@ -10,7 +10,14 @@ class ShippingController < ApplicationController
     logger.info(">>>>>>>>> MEM: #{params}")
     shipping = Shipping.new(shipment_params)
     shipping.save
-    render json: { "id": shipping.id }, status: :created
+
+    shipping_destination = shipping.destination(shipping.country, shipping.state, shipping.city, shipping.zip)
+
+    shipping_packages = shipping.create_packages(shipping.weights)
+
+    shipping_rates = shipping.ups
+
+    render json: { "id": shipping.id, "shipping_rates": shipping_rates }, status: :created
   end
 
   private
--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,11 +1,11 @@
-  require 'active_shipping'
+require 'active_shipping'
 
 class Shipping < ActiveRecord::Base
-    # attr_reader :
+  attr_reader :package, :origin, :destination
+
   ORIGIN = ActiveShipping::Location.new(country: 'US', state: 'WA', city: 'Seattle', zip: '98122')
 
 
-  # destination_hash comes from petsy ex: {country: '', state: '', city: '', zip: ''}
   def destination(country, state, city, zip)
     address = {country: "#{country}", state: "#{state}", city: "#{city}", zip: "#{zip}"}
     ActiveShipping::Location.new(address)
@@ -22,39 +22,39 @@ class Shipping < ActiveRecord::Base
   end
 
 
-  def ups(origin, destination, packages)
+  def ups
 
-  ups = ActiveShipping::UPS.new(
-       login: "shopifolk",
-       password: "Shopify_rocks", key: "7CE85DED4C9D07AB")
+    ups = ActiveShipping::UPS.new(
+    login: "shopifolk",
+    password: "Shopify_rocks", key: "7CE85DED4C9D07AB")
 
-   response = ups.find_rates(origin, destination, packages)
+    response = ups.find_rates(origin, destination, packages)
 
-     ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
+    ups_rates = response.rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price]}
     return ups_rates
   end
 
 
-  def usps(origin, destination, packages)
+  def usps
     developer_key = "677JADED7283"
     usps = ActiveShipping::USPS.new(login: developer_key)
     response = usps.find_rates(origin, destination, packages)
     usps_rates = response.rates.sort_by(&:price).collect {|rate|
-       [rate.service_name, rate.price]}
-    return usps_rates
-  end
+      [rate.service_name, rate.price]}
+      return usps_rates
+    end
 
-  def shipping_options
-
-    package_destination = a.destination(a.country, a.state, a.city, a.zip)
-
-    collected_packages = a.create_packages(a.weights)
-
-    usps_rates = a.usps(origin, package_destination, collected_packages)
-    # => will return an array of arrays
-
-    ups_rates = a.ups.(origin, package_destination, collected_packages)
-    # => returns
-    returns ">>>>>>>>>>>#{usps_rates} + #{ups_rates}"
-  end
+  # def shipping_options
+  # trying to write a method that would run all these methods above
+  #   package_destination = self.destination(self.country, self.state, self.city, self.zip)
+  #
+  #   collected_packages = self.create_packages(self.weights)
+  #
+  #   usps_rates = self.usps(origin, package_destination, collected_packages)
+  #   # => will return an array of arrays
+  #
+  #   ups_rates = self.ups.(origin, package_destination, collected_packages)
+  #   # => returns
+  #   returns ">>>>>>>>>>>#{usps_rates} + #{ups_rates}"
+  # end
 end
