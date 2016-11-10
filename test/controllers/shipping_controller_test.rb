1982ffab0747eed3ee6ad66cdb4e8209ab0c3c48
laurenfb <laurenfriesbrink@gmail.com>
2016-11-10 09:10:03 -0800

    start of tests in shipping controller

--- test/controllers/shipping_controller_test.rb
+++ test/controllers/shipping_controller_test.rb
@@ -4,4 +4,23 @@ class ShippingControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @params = {weight: 100, height: 10, length: 10, width: 10, country: "USA", city: "cat", state: "MI", zip: 29393}
+  end
+
+  test "quote returns json" do
+    post :quote, packages: @params
+    assert_match 'application/json', response.header['Content-Type']
+  end
+
+  test 'posting a quote returns a 201 created?' do
+    post :quote, packages: @params
+    assert_response :created
+  end
+
+  test 'when params is missing something, should return ?? ' do
+    @params[:weight] = nil
+    post :quote, packages: @params
+    assert_response :bad_request
+  end
 end
