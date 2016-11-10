649337b1267750998fe64d44b4a30f508da2a53b
laurenfb <laurenfriesbrink@gmail.com>
2016-11-10 13:37:34 -0800

    first few tests for shippingcontroller

--- test/controllers/shipping_controller_test.rb
+++ test/controllers/shipping_controller_test.rb
@@ -1,26 +1,51 @@
 require 'test_helper'
+require 'json'
 
 class ShippingControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  setup do
+     @request.headers['Accept'] = Mime::JSON
+     @request.headers['Content-Type'] = Mime::JSON.to_s
+   end
+
   def setup
-    @params = {weight: 100, height: 10, length: 10, width: 10, country: "USA", city: "cat", state: "MI", zip: 29393}
+    @params = {:carrier => 'usps',
+      packages: [{weight: 100, height: 10, length: 10, width: 10}],
+      country: "USA",
+      city: "cat",
+      state: "MI",
+      zip: 98119}
   end
 
   test "quote returns json" do
-    post :quote, packages: @params
+    post :quote, @params
     assert_match 'application/json', response.header['Content-Type']
   end
 
-  test 'posting a quote returns a 201 created?' do
-    post :quote, packages: @params
+  test 'posting a quote returns a 201 created' do
+    post :quote, @params
     assert_response :created
   end
 
-  test 'when params is missing something, should return ?? ' do
-    @params[:weight] = nil
-    post :quote, packages: @params
+  test 'when params is missing carrier, should return 400 bad request with meaningful error message' do
+    @params[:carrier] = nil
+    post :quote, @params
     assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "must specify carrier field")
   end
+
+  test 'when params has unsupported carrier, should return 400 bad request with meaningful error message' do
+    @params[:carrier] = "cats"
+    post :quote, @params
+    assert_response :bad_request
+    body = JSON.parse(response.body)
+    assert_instance_of(Hash, body)
+    assert_equal(body["error"], "must specify a carrier from #{ShipWrapper.valid_carriers.join(', ')}")
+  end
+
 end
