fc05c45aa79cf6b2e00bad8ebc489133012b2762
Qlwentt <qlwentt@gmail.com>
2016-11-10 09:08:14 -0800

    set up for testing

--- test/controllers/shipping_services_controller_test.rb
+++ test/controllers/shipping_services_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class ShippingServicesControllerTest < ActionController::TestCase
+	setup do
+     @request.headers['Accept'] = Mime::JSON
+     @request.headers['Content-Type'] = Mime::JSON.to_s
+  	end
+
   test "should get show" do
     get :show
     assert_response :success
