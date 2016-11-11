b7a3c98352cebb6faaa9a54b49c0bb3385e99299
Jennifer Owens <jmo.jennifer@gmail.com>
2016-11-11 13:45:24 -0800

    Change to aid Heroku deployment pt 5

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,4 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
-YAML.load_file("#{::Rails.root}/config/env_provider.yml")[::Rails.env].each {|k,v| ENV[k] = v }
 Rails.application.initialize!
