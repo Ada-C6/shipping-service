7c8ba266b32425898e3de7ea7f88d0eec956a9a8
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-12 10:26:08 -0800

    updated what happens in our controller in a negative case, renders nothing. wrote three tests that now pass testing the positive case of our index controller, using our cassette.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -12,7 +12,7 @@ class QuotesController < ApplicationController
           render json: { "quotes": quote.all_quotes }, status: :created
         }
       rescue ActiveShipping::ResponseError
-        render json: {}, status: :not_found
+        render status: :not_found, nothing: true
       end
     end
 
@@ -23,21 +23,4 @@ class QuotesController < ApplicationController
         # ^^ we are hard coding these below to get our controller to work - these will normally come in from petsy's api-wrapper
       { weight: 15, country: 'US', state: 'OH', city: 'Akron', zip: '44333' }
     end
-
 end
-
-# # Pet data from the user looks like
-# # { "pet": {"name": "fido", "age": 3, "human": "ada"}}
-# def create
-#     logger.info(">>>>> DPR: #{request.body.read}")
-#     logger.info(">>>>> DPR: #{params}")
-#     pet = Pet.new(pet_params)
-#     pet.save
-#     render json: { "id": pet.id }, status: :created
-# end
-#
-# private
-# def pet_params
-#     params.require(:pet).permit(:name, :age, :human)
-# end
-# end
--- test/controllers/quotes_controller_test.rb
+++ test/controllers/quotes_controller_test.rb
@@ -1,38 +1,43 @@
 require 'test_helper'
 
 class QuotesControllerTest < ActionController::TestCase
+    # required setup to make sure we support the API JSON type...
     setup do
         @request.headers['Accept'] = Mime::JSON
         @request.headers['Content-Type'] = Mime::JSON.to_s
     end
 
     test "can get #index" do
-        get :index
-        assert_response :success
+        VCR.use_cassette "test_shipments" do
+            get :index
+            assert_response :success
+        end
     end
 
     test "#index returns json" do
-        get :index
-        assert_match 'application/json', response.header['Content-Type']
+        VCR.use_cassette "test_shipments" do
+            get :index
+            assert_match 'application/json', response.header['Content-Type']
+        end
     end
 
-    # test "#index returns an Array of Quotes objects" do
-    #     get :index
-    #     # Assign the result of the response from the controller action
-    #     body = JSON.parse(response.body)
-    #     assert_instance_of Array, body
-    # end
-    #
-    # test "returns three quote objects" do
-    #     get :index
-    #     body = JSON.parse(response.body)
-    #     assert_equal 3, body.length
-    # end
-    #
-    # test "each quote object contains the relevant keys" do
-    #     keys = %w( age human id name )
-    #     get :index
-    #     body = JSON.parse(response.body)
-    #     assert_equal keys, body.map(&:keys).flatten.uniq.sort
-    # end
+    test "#index returns a Hash with a 'quotes' key that has a value that is an Array of Arrays, with each inner array having two elements, the carrier/method and the price of that specific quote (in cents)" do
+        VCR.use_cassette "test_shipments" do
+            get :index
+            # Assign the result of the response from the controller action
+            body = JSON.parse(response.body)
+            assert_instance_of Hash, body
+            hash_value = body["quotes"]
+
+            hash_value.each do |element|
+                assert_instance_of Array, element
+                assert_instance_of String, element[0]
+                # testing that each of these carrier/method elements has a 'PS' in them, for either UPS or USPS:
+                assert_equal true, element[0].include?("PS")
+                assert_instance_of Fixnum, element[1]
+            end
+        end
+    end
+
+    
 end
